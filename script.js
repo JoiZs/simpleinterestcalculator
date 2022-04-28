0634fe5a797f5bfa5c585d4f78ffdb7e67ef5f1d
JoiZs <pyaesoneaung1126@hotmail.com>
2022-04-28 04:48:18 +0000

    add more style

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function compute()
     }else{
         // If inputs are valid, the calculation steps will process
         document.getElementById("result").innerHTML = `
-            If you deposit <mark>${principal}</mark>,
-            at an interest rate of  <mark>${rate} % </mark>.
-            You will receive an amount of <mark>${interest}</mark>,
+              If you deposit <mark>${principal}</mark>,<br>
+            at an interest rate of  <mark>${rate} % </mark>.<br>
+            You will receive an amount of <mark>${interest}</mark>,<br>
             in the year <mark>${year}</mark>
             `
         }
@@ -27,6 +27,6 @@ function compute()
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerHTML = rateval
+    document.getElementById("rate_val").innerHTML = rateval + " %"
 }
         
